2c9e3e7b0d4dcda31cc7624653987e62cd8566a1
Sarthak Dwivedi <114822434+sarthak7772@users.noreply.github.com>
2024-07-18 10:42:02 +0530

    tuple.py

--- tuple.py
+++ tuple.py
@@ -1,10 +1,10 @@
 t=(1,2,3,4,6,8)
-# for ele in range(-6,0):
-#     print(t[ele],end=',')
+for ele in range(-6,0):
+    print(t[ele],end=',')
     
-# n=len(t)
-# for i in range(0,n):
-#     print(t[n-i-1])
+n=len(t)
+for i in range(0,n):
+    print(t[n-i-1])
      
      
 print(t.sort())
